7a2336dae3565c1c821ae48d60a6ea39a15fdf1a
張育誠 <taco_chang@walsin.com>
2018-12-13 17:11:34 +0800

    Upload Demo Page

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const ProvidePlugin = require('webpack').ProvidePlugin;
 const path = require('path');
 
 module.exports = {
+  baseUrl: '/tabaco-vue-material/',
   outputDir: path.resolve(__dirname, 'docs'),
   devServer: {
     port: 3000
